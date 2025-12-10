da8b2c3c56408c1a547e7122f82f9ae1b2b337a9
Elias-afk867 <eg1156744@gmail.com>
2025-12-10 15:52:01 -0600

    app.php
    igualmente, se agrego la ruta de la api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,26 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // Asegurar que las excepciones de validación devuelvan JSON en rutas API
+        $exceptions->shouldRenderJsonWhen(function ($request, Throwable $e) {
+            if ($request->is('api/*')) {
+                return true;
+            }
+            return $request->expectsJson();
+        });
     })->create();
